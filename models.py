8aa9fc8028f029cb7a4116ed3fb9a3cf2cbcb4e0
Jayaprakash <jayaprakash@irmai.io>
2026-02-24 17:34:24 +0000

    Delete All

--- models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
-from database import Base
-
-class Todo(Base):
-    __tablename__ = "todos"
-    id = Column(Integer, primary_key=True, index=True)
-    task = Column(String)
-    completed = Column(Boolean, default=False)
-    completed_at = Column(DateTime, nullable=True)
